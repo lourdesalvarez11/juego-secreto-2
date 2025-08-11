39fcf5b360190069c1fdcd93964de0d2772d4d7a
Lourdes <lourdesalvarez011@outlook.com>
2025-08-11 16:31:51 -0300

    Version final juego 2
    html

---
+++ app.js
@@ -0,0 +1,74 @@
+let numeroSecreto = 0; // variable para guardar el numero secreto
+let intentos=0;
+let listaNumerosSecretos = [];
+let numeroMaximo = 10;
+console.log(numeroSecreto);
+
+function generarNumeroSecreto() {
+  let numeroGenerado = Math.floor(Math.random()*numeroMaximo)+1;
+  //si el numero generado esta incluido en la lista
+  console.log(numeroGenerado);
+  console.log(listaNumerosSecretos);
+  // si toddos los numeros ya estan sorteados
+  if (listaNumerosSecretos.length == numeroMaximo) {
+    asignarTextoElemento("p","Todos los números secretos ya han sido sorteados");
+  } else {
+      if (listaNumerosSecretos.includes(numeroGenerado)) {
+        //vuelve a generar un numero
+        return generarNumeroSecreto();
+    } else{
+        listaNumerosSecretos.push(numeroGenerado); //agrega el numero generado a la lista
+        return numeroGenerado;
+    }
+  } 
+}
+
+function asignarTextoElemento(elemento,texto){
+    let elementoHTML=document.querySelector(elemento);
+    elementoHTML.innerHTML=texto;
+    return;
+}
+
+function verificarIntento() {
+   let numeroDeUsuario = parseInt(document.getElementById("valorUsuario").value);
+   //console.log(intentos);
+   if (numeroDeUsuario == numeroSecreto){
+    asignarTextoElemento("p",`¡Felicidades! Has acertado el número en ${intentos} ${(intentos ===1) ? "vez" : "veces"}`)
+    document.getElementById("reiniciar").removeAttribute("disabled")   // habilita el boton reiniciar del html
+   }else{
+    //el usario no acerto
+    if(numeroDeUsuario > numeroSecreto){
+        asignarTextoElemento("p","El número secreto es menor");
+   } else{
+        asignarTextoElemento("p","El número secreto es mayor");}
+        intentos++;
+        limpiarCaja(); // llama la funcion limpiarCaja
+   }
+    return;
+}
+function limpiarCaja() {
+   document.querySelector("#valorUsuario").value="";// otra funcion de usar el id  //let valorCaja = 
+    // valorCaja.value = ""; otra manera // para que quede vacio el input
+    
+}
+function condicionesInicales(){
+    asignarTextoElemento("h1","Juego del número secreto");
+    asignarTextoElemento("p",`Indica un número del 1 al ${numeroMaximo}`);
+    numeroSecreto = generarNumeroSecreto();
+    intentos=1;
+    return;
+}
+
+function reiniciarJuego() {
+    //necesito limpiar la caja
+    limpiarCaja();
+    //indicar mensaje de intervalo de numero
+    //generar numero aleatorio
+    //intentos 
+    condicionesInicales();
+    //deshabilitar boton reiniciar
+    document.querySelector("#reiniciar").setAttribute("disabled", true);
+    return;
+}
+
+condicionesInicales();
